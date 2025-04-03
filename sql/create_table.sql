36eaac63292762c52b36fa8e24ae6804659131d0
阳光阿盖尔 <wj52030000@outlook.com>
2025-04-03 10:47:15 +0800

    增加会员兑换码功能后的sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -18,6 +18,9 @@ create table if not exists user
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
+    vipExpireTime datetime     null comment '会员过期时间',
+    vipCode       varchar(128) null comment '会员兑换码',
+    vipNumber     bigint       null comment '会员编号'
     UNIQUE KEY uk_userAccount (userAccount),
     INDEX idx_userName (userName)
 ) comment '用户' collate = utf8mb4_unicode_ci;
