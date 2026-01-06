fd081751a5c6a86cfc75d42c4fd293e490774496
阳光阿盖尔 <wj52030000@outlook.com>
2026-01-06 16:41:29 +0800

    协同编辑功能修复

--- src/main/java/com/argyle/argylepicturebackend/manager/websocket/disruptor/PictureEditEventWorkHandler.java
+++ src/main/java/com/argyle/argylepicturebackend/manager/websocket/disruptor/PictureEditEventWorkHandler.java
@@ -8,7 +8,7 @@ import com.argyle.argylepicturebackend.manager.websocket.model.PictureEditRespon
 import com.argyle.argylepicturebackend.model.entity.User;
 import com.argyle.argylepicturebackend.service.UserService;
 import com.lmax.disruptor.WorkHandler;
-import groovy.lang.Lazy;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -16,43 +16,47 @@ import org.springframework.web.socket.WebSocketSession;
 
 import javax.annotation.Resource;
 
-@Slf4j
+/**
+ * 图片编辑事件处理器（消费者）
+ */
 @Component
+@Slf4j
 public class PictureEditEventWorkHandler implements WorkHandler<PictureEditEvent> {
 
     @Resource
-    @Lazy
     private PictureEditHandler pictureEditHandler;
 
     @Resource
     private UserService userService;
 
     @Override
-    public void onEvent(PictureEditEvent event) throws Exception {
-        PictureEditRequestMessage pictureEditRequestMessage = event.getPictureEditRequestMessage();
-        WebSocketSession session = event.getSession();
-        User user = event.getUser();
-        Long pictureId = event.getPictureId();
+    public void onEvent(PictureEditEvent pictureEditEvent) throws Exception {
+        PictureEditRequestMessage pictureEditRequestMessage = pictureEditEvent.getPictureEditRequestMessage();
+        WebSocketSession session = pictureEditEvent.getSession();
+        User user = pictureEditEvent.getUser();
+        Long pictureId = pictureEditEvent.getPictureId();
         // 获取到消息类别
         String type = pictureEditRequestMessage.getType();
-        PictureEditMessageTypeEnum pictureEditMessageTypeEnum = PictureEditMessageTypeEnum.valueOf(type);
-        // 调用对应的消息处理方法
+        PictureEditMessageTypeEnum pictureEditMessageTypeEnum = PictureEditMessageTypeEnum.getEnumByValue(type);
+        // 根据消息类型处理消息
         switch (pictureEditMessageTypeEnum) {
             case ENTER_EDIT:
-                pictureEditHandler.handleEnterEditMessage(session, pictureEditRequestMessage, pictureId, user);
-                break;
-            case EDIT_ACTION:
-                pictureEditHandler.handleEditActionMessage(session, pictureEditRequestMessage, pictureId, user);
+                pictureEditHandler.handleEnterEditMessage(pictureEditRequestMessage, session, user, pictureId);
                 break;
             case EXIT_EDIT:
-                pictureEditHandler.handleExitEditMessage(session, pictureEditRequestMessage, pictureId, user);
+                pictureEditHandler.handleExitEditMessage(pictureEditRequestMessage, session, user, pictureId);
+                break;
+            case EDIT_ACTION:
+                pictureEditHandler.handleEditActionMessage(pictureEditRequestMessage, session, user, pictureId);
                 break;
             default:
+                // 其他消息类型，返回错误提示
                 PictureEditResponseMessage pictureEditResponseMessage = new PictureEditResponseMessage();
                 pictureEditResponseMessage.setType(PictureEditMessageTypeEnum.ERROR.getValue());
                 pictureEditResponseMessage.setMessage("消息类型错误");
                 pictureEditResponseMessage.setUser(userService.getUserVO(user));
                 session.sendMessage(new TextMessage(JSONUtil.toJsonStr(pictureEditResponseMessage)));
+                break;
         }
     }
 }
